c1bef94bedac47cb64a37bb0b6152e3b8e9b4b91
Maria Milosh <mariamilosh@gmail.com>
2021-12-03 00:02:02 -0500

    Completed methods exercises in intro ruby book

---
+++ methods_exercises.rb
@@ -0,0 +1,40 @@
+puts "Exercise 1"
+
+def greeting(name)
+  return "Hello, #{name}"
+end
+puts greeting("Maria")
+
+#Exercise 2
+# x = 2
+# puts x = 2
+# p name = "Joe"
+# four = "four"
+# print something = "nothing"
+
+puts "Exercise 3"
+def multiply(a, b)
+  a * b
+end
+puts multiply(2, 4)
+
+#Exercise 4
+# returns nothing
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+puts "Exercise 5"
+# returns nothing
+def scream(words)
+  words = words + "!!!!"
+  # return
+  puts words
+end
+
+scream("Yippeee")
+
