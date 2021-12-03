aecc0236216dfec4d8e398c5a03a6fdd267293b2
Maria Milosh <mariamilosh@gmail.com>
2021-12-02 22:35:01 -0500

    Completed variables exercises in ruby intro book

---
+++ age.rb
@@ -0,0 +1,9 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+
+years = 10
+while years <= 40 do
+  puts "In #{years} years you will be:"
+  puts age + years
+  years += 10
+end 
---
+++ name.rb
@@ -0,0 +1,15 @@
+puts "Exercise 1"
+puts "Type your name"
+name = gets.chomp
+
+puts "Hello #{name}"
+puts "Exercise 3"
+10.times { puts name }
+
+puts "Exercise 4"
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+
+puts "#{first_name} #{last_name}"
